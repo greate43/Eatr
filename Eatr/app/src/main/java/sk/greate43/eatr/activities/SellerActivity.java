23087770f8d851d9fd7327aa18b81bae292a3ec9
SALMAN KHAN <greate43.sk@gmail.com>
2017-11-02 16:33:47 +0500

    Bottom tabs and floating action button added

--- Eatr/app/src/main/java/sk/greate43/eatr/activities/SellerActivity.java
+++ Eatr/app/src/main/java/sk/greate43/eatr/activities/SellerActivity.java
@@ -3,15 +3,14 @@ package sk.greate43.eatr.activities;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import sk.greate43.eatr.R;
+import sk.greate43.eatr.fragments.SellFoodFragment;
 
 public class SellerActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,13 +19,16 @@ public class SellerActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    FragmentManager fragmentManager = getSupportFragmentManager();
+
+                    SellFoodFragment sellFoodFragment=new SellFoodFragment();
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.activity_seller_fragment_container, sellFoodFragment)
+                            .commit();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -38,8 +40,9 @@ public class SellerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
