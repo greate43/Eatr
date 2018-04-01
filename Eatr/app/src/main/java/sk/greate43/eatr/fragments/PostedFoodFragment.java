e97288118d307833f2c091a760487cc47eee1cd4
SALMAN KHAN <greate43.sk@gmail.com>
2018-04-01 16:38:19 +0500

    updated

--- Eatr/app/src/main/java/sk/greate43/eatr/fragments/PostedFoodFragment.java
+++ Eatr/app/src/main/java/sk/greate43/eatr/fragments/PostedFoodFragment.java
@@ -1,11 +1,11 @@
 package sk.greate43.eatr.fragments;
 
+import android.support.v4.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -37,6 +37,8 @@ import sk.greate43.eatr.adaptors.PostedFoodRecyclerViewAdaptor;
 import sk.greate43.eatr.entities.Food;
 import sk.greate43.eatr.holders.PostedFoodViewHolder;
 import sk.greate43.eatr.utils.Constants;
+
+
 
 
 public class PostedFoodFragment extends Fragment implements PostedFoodViewHolder.EditPostedFood {
