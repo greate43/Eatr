ce192de53258765b24f554e28c65eb11452da49c
ummarhussain <ummarkashmiri@gmail.com>
2018-04-02 23:35:02 +0500

    commit
    aaa

--- Eatr/app/src/main/java/sk/greate43/eatr/fragments/ProfileFragment.java
+++ Eatr/app/src/main/java/sk/greate43/eatr/fragments/ProfileFragment.java
@@ -465,6 +465,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onPrepareOptionsMenu(Menu menu) {
         MenuItem search = menu.findItem(R.id.menu_item_search);
+        
         search.setVisible(false);
 
         super.onPrepareOptionsMenu(menu);
